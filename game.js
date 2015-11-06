b21ba1ba2c103042b831f022b0f84a2a9af7b15c
Danielle H <daheber@gmail.com>
2015-11-06 10:57:25 -0800

    First draft of guessing game

--- game.js
+++ game.js
@@ -1,7 +1,21 @@
-function startGame() {
+function playGame() {
 
-  alert("Testing");
-
+    var correctGuess = false;
+    var validInterest = new Array("Bicycling", "Reading", "Walking my dog", "Daydreaming");
+    while (!correctGuess){
+        var userGuess = prompt("Enter one of my favorite activities: ", "");
+        for (var index = 0; index < validInterest.length; index++){
+          if (userGuess == validInterest[index]){
+              correctGuess = true;
+          }
+        }
+        if (correctGuess) {
+          alert("Yay! You got it correct.");
+        }
+        else {
+          alert("Nice guess, but that's not one of my favorite activities. Try Again.");
+        }
+    }
 }
 
 
