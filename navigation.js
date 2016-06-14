1f1b3373e5e6aebfaf3ad523fa9a7dd3b5e72ac2
Danielle H <daheber@gmail.com>
2016-06-13 19:21:07 -0700

    active link highlighted in footer nav

---
+++ navigation.js
@@ -0,0 +1,16 @@
+//active link color change
+var pageURL = window.location.pathname;
+pageURL = pageURL.replace(".html", "");
+pageURL = pageURL.replace("/", "");
+if(pageURL === "index"){
+  pageURL = "home";
+}
+
+var navLinks = document.querySelectorAll(".footerNav li a");
+for(var i=0; i<navLinks.length; i++){
+  var currentNavLink = navLinks[i].textContent.toLowerCase();
+  if(pageURL === currentNavLink){
+    navLinks[i].classList.remove("active"); //for later when it's a SPA
+    navLinks[i].classList.add("active");
+  }
+}
