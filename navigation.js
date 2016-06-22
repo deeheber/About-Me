93803ddb94681007fdaeacfd99b1014f8e41eee2
Danielle H <daheber@gmail.com>
2016-06-22 14:48:00 -0700

    active link color for issue #15

--- navigation.js
+++ navigation.js
@@ -1,9 +1,12 @@
-//initial page load
+/*** initial page load ***/
 $(document).ready(function() {
+    //display the home section
     $('#home').show();
+    //active link color on first menu item in the nav
+    $('.menu li:first-child a').addClass('active');
 });
 
-//navigation open/close
+/*** navigation open/close and events ***/
 function openMenu(){
   $('.menu').fadeIn(500);
   $('.menu li').show();
@@ -21,38 +24,25 @@ function closeMenu(){
 $('.cross').on('click', closeMenu);
 $('.hamburger').on('click', openMenu);
 
-//navigation link events
 $('.menu li a').on('click', function(event){
   event.preventDefault();
 
   $('section').hide();
 
-  var clickedLink = $(this).data('link');
-
+  //display the selected section
+  var clickedLinkName = $(this).data('link');
   $('section').each(function(index, el){
       var currentSection = $(this);
-    if(currentSection.attr('id') == clickedLink){
+    if(currentSection.attr('id') == clickedLinkName){
       currentSection.show();
     }
   });
 
+  //update active link color
+  $('.menu li a').each(function(){
+    $(this).removeClass('active');
+  });
+  $(this).addClass('active');
+
   closeMenu();
 });
-
-
-//active link color change
-var pageURL = window.location.pathname;
-pageURL = pageURL.replace(".html", "");
-pageURL = pageURL.replace("/", "");
-if(pageURL === "index"){
-  pageURL = "home";
-}
-
-var navLinks = document.querySelectorAll(".menu li a");
-for(var i=0; i<navLinks.length; i++){
-  var currentNavLink = navLinks[i].textContent.toLowerCase();
-  if(pageURL === currentNavLink){
-    navLinks[i].classList.remove("active"); //for later when it's a SPA
-    navLinks[i].classList.add("active");
-  }
-}
