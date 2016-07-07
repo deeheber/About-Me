bfed293597449b80edc5b72b1d73a361fc1b1e55
Danielle H <daheber@gmail.com>
2016-07-07 11:54:29 -0700

    router initial set up complete

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -3,7 +3,45 @@
 
   controllers.home = function(ctx, next){
     console.log("home controller fired");
+    $('section').hide();
     $('#home').show();
+    closeMenu();
+
+    //update the active link
+  };
+
+  controllers.contact = function(ctx, next){
+    console.log("contact controller fired");
+    $('section').hide();
+    $('#contact').show();
+    closeMenu();
+
+    //update the active link
+  };
+
+  controllers.interests = function(ctx, next){
+    console.log("interests controller fired");
+    $('section').hide();
+    $('#interests').show();
+    closeMenu();
+
+    //update the active link
+  };
+
+  controllers.game = function(ctx, next){
+    console.log("game controller fired");
+    $('section').hide();
+    $('#game').show();
+    closeMenu();
+
+    //update the active link
+  };
+
+  controllers.notFound = function(ctx, next){
+    console.log("notFound controller fired");
+    $('section').hide();
+    $('#notFound').show();
+    closeMenu();
   };
 
 
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,4 +1,9 @@
 /*** Home ***/
 page('/', controllers.home);
+page('/contact', controllers.contact);
+page('/interests', controllers.interests);
+page('/game', controllers.game);
+
+page('*', controllers.notFound);
 
 page();
