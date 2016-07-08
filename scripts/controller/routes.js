d55cef650e208660dbcd811a4ccbebfdd57bd4b8
Danielle H <daheber@gmail.com>
2016-07-07 18:12:20 -0700

    temporarily removing the 404 controller

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -4,6 +4,6 @@ page('/About-Me/contact', controllers.contact);
 page('/About-Me/interests', controllers.interests);
 page('/About-Me/game', controllers.game);
 
-page('*', controllers.notFound);
+//page('*', controllers.notFound);
 
 page();
